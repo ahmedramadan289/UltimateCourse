6fa68bcfae8175fdacf04ce8eebb953b3e709d19
ahmedramadan289 <rampomedo98@gmail.com>
2020-05-16 18:21:57 +0200

    modifie files

--- stage/js/script.js
+++ stage/js/script.js
@@ -1,3 +1,11 @@
 $(function() {
-    $('.content-area').width($(window).width() - $('.sidebar').innerWidth());
+    'use strict';
+    $('.content-area').width($(window).width() - $('.sidebar').width() - 40);
+    $(window).resize(function() {
+        $('.content-area').width($(window).width() - $('.sidebar').width() - 40);
+
+    });
+    $(".toggle-sidebar").on("click", () => {
+        $('.content-area , .sidebar').toggleClass('no-sidebar')
+    });
 });
